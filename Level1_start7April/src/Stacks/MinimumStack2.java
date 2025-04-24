f5a03025c8f25e67c283c1d53b383f2ffa1f9de3
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2025-04-25 00:22:33 +0530

    done stack ques on 24th april

--- Level1_start7April/src/Stacks/MinimumStack2.java
+++ Level1_start7April/src/Stacks/MinimumStack2.java
@@ -1,4 +1,51 @@
 package Stacks;
 
+import java.util.Stack;
+
 public class MinimumStack2 {
+
+    Stack<Long> stack;
+    long min;
+
+    public MinimumStack2() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(0L); // diff = val - min = 0
+            min = val;
+        } else {
+            stack.push((long) val - min);
+            if (val < min) {
+                min = val;
+            }
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) return;
+
+        long diff = stack.pop();
+        if (diff < 0) {
+            min = min - diff; // undo the change
+        }
+    }
+
+    public int top() {
+        if (stack.isEmpty()) return -1;
+
+        long diff = stack.peek();
+        if (diff >= 0) {
+            return (int) (min + diff);
+        } else {
+            return (int) min;
+        }
+    }
+
+    public int getMin() {
+        if (stack.isEmpty()) return -1;
+        return (int) min;
+    }
 }
+
